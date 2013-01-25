1ee2d97c0e910ffb9a8ae16e43661fc28c09390f
autoway <autoway@autoway-PC>
2013-01-25 08:56:34 +0800

    New branch
    2013-1-25

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is cool3");
+    System.out.println("2013-1/25: Git is cool3");
   }
 } 
