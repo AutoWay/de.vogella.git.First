acbcb4bcfc85ab4ff569ca074c210b7eaed54baa
AutoWay <autoway.abc@gmail.com>
2013-01-31 11:03:40 +0800

    Add power operation

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,5 +3,6 @@ package de.vogella.git.first;
 public class GitTest {
   public static void main(String[] args) {
     System.out.println("2013-1/25: Git is cool3");
+    new Power();
   }
 } 
---
+++ de.vogella.git.first/src/de/vogella/git/first/Power.java
@@ -0,0 +1,13 @@
+package de.vogella.git.first;
+
+public class Power {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
